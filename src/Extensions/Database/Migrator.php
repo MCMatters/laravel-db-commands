491db76176c0b8fbd3b4e2595c04c50fddfe21f9
Dmitry Borzyonok <dima.matters@gmail.com>
2019-02-06 12:25:19 +0200

    Typo fix

--- src/Extensions/Database/Migrator.php
+++ src/Extensions/Database/Migrator.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace McMatters\LaravelDbCommands\Extentions\Database;
+namespace McMatters\LaravelDbCommands\Extensions\Database;
 
 use Illuminate\Database\Migrations\Migrator as BaseMigrator;
 use const PATHINFO_FILENAME;
@@ -11,7 +11,7 @@ use function pathinfo;
 /**
  * Class Migrator
  *
- * @package McMatters\LaravelDbCommands\Extentions\Database
+ * @package McMatters\LaravelDbCommands\Extensions\Database
  */
 class Migrator extends BaseMigrator
 {
